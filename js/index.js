b49c67947e713281e63d05ae91aafc77e1e6a941
Al2003x <Danilovvik@bk.ru>
2022-02-25 22:14:08 +0300

    feat: доделал верску

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
-new Swiper('.reviews__slider', {
-    slidesPerView: 1,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-})
+let reviews = new Swiper(".reviews__slider", {
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+let design = new Swiper(".design__slider", {
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  loop: true,
+  slideActiveClass: "swiper-slide-active-mod",
+  spaceBetween: 22,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
